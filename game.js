ebde07df947e4e5114e76b8d79e6585cc8c5e3a0
Tia <tianxcheng888@gmail.com>
2024-12-31 11:22:19 -0800

    add esay/medium/hard mode, hint button, theme button

--- game.js
+++ game.js
@@ -3,94 +3,135 @@ var gamePattern = [];
 
 var level = 0;
 var started = false;
+var score = 0;
+var difficulty = "Medium";
+var hintCount = 3;
+var isLightMode = false;
 
 var buttonColours = ["red", "blue", "green", "yellow"];
 
-// input color, 
-// output pressed effect, stop pressed effect for 100 ms
-function animatePress(currentColour){
+function animatePress(currentColour) {
     $("#" + currentColour).addClass("pressed");
-    setTimeout(function(){
+    setTimeout(function() {
         $("#" + currentColour).removeClass("pressed");
     }, 100);
 }
 
-//input color, output sound
-function playSound(name){
+function playSound(name) {
     var audio = new Audio("./sounds/" + name + ".mp3");
     audio.play();
 }
 
-/** get the next random color
- * add this color into gamepattern
- * fadein fadeout effect, sound for this random color
- * userClickPattern has to reset everytime
- */
-function nextSequence(){
+function updateScore() {
+    $("#score").text("Score: " + score);
+}
+
+function updateHints() {
+    $("#hint-count").text("Hints Left: " + hintCount);
+}
+
+function setDifficulty(level) {
+    difficulty = level;
+    $(".difficulty-btn").removeClass("active");
+    $("#" + level.toLowerCase()).addClass("active");
+}
+
+function nextSequence() {
     userClickedPattern = [];
     level++;
+    score++;
+    updateScore();
+
     $("#level-title").text("Level " + level);
     var randomNumber = Math.floor(Math.random() * 4);
     var randomChosenColour = buttonColours[randomNumber];
     gamePattern.push(randomChosenColour);
 
+    let delay = difficulty === "Easy" ? 800 : difficulty === "Medium" ? 500 : 300;
+
     $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
     playSound(randomChosenColour);
-    
+
+    setTimeout(function() {
+        $("#" + randomChosenColour).fadeOut(delay).fadeIn(100);
+    }, delay);
 }
 
-/** detect when any button is clicked 
- * get the color
- * add this color into useClickedPattern array
- * play the sound of this color
- * */
-$(".btn").click(function(){
+$(".btn").click(function() {
     var userChosenColour = $(this).attr("id");
     userClickedPattern.push(userChosenColour);
     playSound(userChosenColour);
-
-    // animate the clicked color
     animatePress(userChosenColour);
-
-    //check answer
-    checkAnswer(userClickedPattern.length -1);
+    checkAnswer(userClickedPattern.length - 1);
 });
 
-//detect a keyboard key has been pressed
-$(document).keypress(function(){
-    if(!started){
+$(document).keypress(function() {
+    if (!started) {
         $("#level-title").text("Level " + level);
         nextSequence();
         started = true;
     }
 });
 
-//check if the user is correct
-function checkAnswer(currentLevel){
-    if(gamePattern[currentLevel] === userClickedPattern[currentLevel]){
-        if(gamePattern.length === userClickedPattern.length){
-            setTimeout(function(){
+function checkAnswer(currentLevel) {
+    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+        if (gamePattern.length === userClickedPattern.length) {
+            setTimeout(function() {
                 nextSequence();
             }, 1000);
         }
-    }else{
+    } else {
         playSound("wrong");
-
-        //apply "game-over"class to the body
         $("body").addClass("game-over");
-        $("#level-title").text("Game OVer. Press Any Key to Restart");
-        setTimeout(function(){
+        $("#level-title").text("Game Over. Press Any Key to Restart");
+        setTimeout(function() {
             $("body").removeClass("game-over");
         }, 200);
-
         startOver();
-
-
     }
 }
 
-function startOver(){
+function startOver() {
     level = 0;
+    score = 0;
+    hintCount = 3;
+    updateHints();
     gamePattern = [];
     started = false;
 }
+
+$("#easy, #medium, #hard").click(function() {
+    setDifficulty($(this).attr("id"));
+});
+
+$("#hint-button").click(function() {
+    if (hintCount > 0) {
+        hintCount--;
+        updateHints();
+        playCurrentSequence();
+    } else {
+        alert("No hints left!");
+    }
+});
+
+function playCurrentSequence() {
+    let delay = 0;
+    gamePattern.forEach((color) => {
+        setTimeout(function() {
+            $("#" + color).fadeIn(100).fadeOut(100).fadeIn(100);
+            playSound(color);
+        }, delay);
+        delay += 600;
+    });
+}
+
+$("#theme-button").click(function() {
+    isLightMode = !isLightMode;
+    if (isLightMode) {
+        $("body").addClass("light-mode");
+        $("#theme-button").text("Switch to Dark Mode");
+    } else {
+        $("body").removeClass("light-mode");
+        $("#theme-button").text("Switch to Light Mode");
+    }
+});
